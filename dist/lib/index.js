777e121d547dbda5746ec1baf9367e53509763da
Leonardo Rossi <leonardo.rossi@gmail.com>
2022-04-20 08:39:51 +0200

    updates .gitignore and remove dist directory

--- dist/lib/index.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-exports.__esModule = true;
-var VideoDownloader_1 = require("./classes/VideoDownloader");
-module.exports = VideoDownloader_1["default"];
--- dist/tests/VideoDownloader.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-exports.__esModule = true;
-//import { test, expect } from 'jest';
-var VideoDownloader_1 = require("../lib/classes/VideoDownloader");
-test('should convert Vimeo url to embedded url', function () {
-    var downloader = new VideoDownloader_1["default"]({});
-    var convertedUrl = downloader.convertVideoUrlToEmbeddedUrl('https://vimeo.com/123456789');
-    expect('a').toBe('b');
-});
